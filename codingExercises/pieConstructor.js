e54d053b22aab5044238c7aed0fb04552f60fe9c
Nick Martinez <23nicholasmartinez@gmail.com>
2017-08-16 08:32:17 -0700

    Create pie constructor function with delegation

---
+++ codingExercises/pieConstructor.js
@@ -0,0 +1,27 @@
+function Pie ( flavour ) {
+  this.flavour = flavour;
+};
+
+Pie.prototype = {
+  getFlavour () {
+    return this.flavour;
+  },
+};
+
+tape( 'Pie Constructor', t => {
+  let actual, expected;
+  const pie = new Pie( 'pizza' );
+
+  actual = pie.getFlavour();
+  expected = 'pizza';
+  t.equal( actual, expected, 'Pie::getFlavour() should return the pie\'s flavour' );
+
+  // because it used a constructor function
+  t.ok( pie instanceof Pie, 'Pie() should create instances of Pie' );
+
+  // because it used a copy of the prototype
+  const pie2 = Pie( 'apple' );
+  t.equal( pie.getFlavour, pie2.getFlavour, 'Pie() should create shallow copy the the prototype' );
+
+  t.end();
+});
